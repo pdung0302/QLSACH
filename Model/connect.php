b6e8a67b4f6dc2218a91a655cf9f0df2f46dc064
pdung0302 <160315087+pdung0302@users.noreply.github.com>
2024-05-10 16:41:58 +0000

    fix all

---
+++ Model/connect.php
@@ -0,0 +1,26 @@
+<?php
+$env = file_get_contents(".env");
+
+$lines = explode("\n",$env);
+
+foreach($lines as $line){
+  preg_match("/([^#]+)\=(.*)/",$line,$matches);
+  if(isset($matches[2])){
+    putenv(trim($line));
+  }
+} 
+
+$servername = getenv('servername');
+$username =getenv('username');
+$password = getenv('password');
+$database = getenv('database');
+$port = getenv('port');
+
+try {
+    $conn = new PDO("mysql:host=$servername;port=$port;dbname=$database", $username, $password);
+    // Thiết lập chế độ báo lỗi và chế độ tùy chỉnh
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch(PDOException $e) {
+    echo "Kết nối cơ sở dữ liệu thất bại: " . $e->getMessage();
+}
+?>
---
+++ controller/login.php
@@ -0,0 +1,47 @@
+<?php
+$env = file_get_contents("../Model/.env ");
+
+$lines = explode("\n",$env);
+
+foreach($lines as $line){
+  preg_match("/([^#]+)\=(.*)/",$line,$matches);
+  if(isset($matches[2])){
+    putenv(trim($line));
+  }
+} 
+// Kết nối đến cơ sở dữ liệu
+$servername = getenv('servername');
+$username =getenv('username');
+$password = getenv('password');
+$database = getenv('database');
+$port = getenv('port');
+
+try {
+    $conn = new PDO("mysql:host=$servername;port=$port;dbname=$database", $username, $password);
+    // Thiết lập chế độ báo lỗi và chế độ tùy chỉnh
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch(PDOException $e) {
+    echo "Kết nối cơ sở dữ liệu thất bại: " . $e->getMessage();
+}
+
+// Đăng nhập
+if(isset($_POST['login'])) {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    $stmt = $conn->prepare("SELECT * FROM User WHERE TenUser = :username AND MatKhau = :password");
+    $stmt->bindParam(':username', $username);
+    $stmt->bindParam(':password', $password);
+    $stmt->execute();
+
+    if($stmt->rowCount() > 0) {
+        // Đăng nhập thành công, chuyển hướng người dùng đến trang "sach.php"
+        header("Location: ../View/sach.php");
+        exit(); // Dừng script để đảm bảo không có mã HTML nào khác được thực thi
+    } else {
+        echo "Tên đăng nhập hoặc mật khẩu không đúng!";
+    }
+}
+
+
+?>
